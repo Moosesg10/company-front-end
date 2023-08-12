b5f23af011cde8605707a11c27425052b0bee666
Moosesg10 <moisesgak66@gmail.com>
2023-08-12 11:47:21 -0400

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import React, { useState } from 'react'
 import EmployeesCompany from './pages/EmployeesCompany'
 import { Routes, Route, Navigate } from "react-router-dom"
 import LoginComapany from './pages/LoginCompany';
+import { Analytics } from '@vercel/analytics/react';
 function App() {
 
 
 
   return (
+  <>
     <Routes>
        <Route path='/company/admin' element={ <LoginComapany />}/>
         <Route path={`/company/admin/:users`} element={ <EmployeesCompany />}/> 
      
     </Routes>
+    <Analytics/>
+  </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App'
 import './index.css'
 import { ContextProvider } from "./context/FormContext";
 import { BrowserRouter } from "react-router-dom";
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
