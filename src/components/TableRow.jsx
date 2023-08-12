3260080f44a43aa2b0a2ace827b3dc6cf7c4db9a
Moosesg10 <moisesgak66@gmail.com>
2023-08-12 10:04:28 -0400

    cambi

--- src/components/TableRow.jsx
+++ src/components/TableRow.jsx
@@ -1,7 +1,11 @@
 import React, { useContext, useEffect, useState } from "react";
 import FormContext from "../context/FormContext";
-import { asignament, asignamentRol, delet, getAdmin } from "../controllers/Tables.controllers";
-
+import {
+  asignament,
+  asignamentRol,
+  delet,
+  getAdmin,
+} from "../controllers/Tables.controllers";
 
 function TableRow({
   data,
@@ -12,85 +16,105 @@ function TableRow({
   getEmployee,
   params,
 }) {
-//Constantes de Context
-const context = useContext(FormContext);
+  //Constantes de Context
+  const context = useContext(FormContext);
   const row = context.row;
-  const admin = context.admin
-  const setAdmin = context.setAdmin
-  const optionsadmin = context.optionsadmin
-  const options = context.options
-  const optionsemployee = context.optionsemployee
-  const roles = context.roles
-  const setRoles = context.setRoles
-//Constantes Heradadas
+  const admin = context.admin;
+  const setAdmin = context.setAdmin;
+  const optionsadmin = context.optionsadmin;
+  const options = context.options;
+  const optionsemployee = context.optionsemployee;
+  const roles = context.roles;
+  const setRoles = context.setRoles;
+  //Constantes Heradadas
   const { name, salary, rol } = data;
 
-//Constantes De Estados De React
+  //Constantes De Estados De React
   const [mostarOptionsAddRol, setMostarOptionsAddRol] = useState(false);
   const [rols, setRols] = useState(false);
 
- 
+  const sal = "" + salary;
+  console.log(sal.length);
 
-//Funciones de Manejo de Roles 
+  let newSalary;
+
+  switch (sal.length) {
+    case 5:
+      newSalary = sal[0] + sal[1] + "." + sal[2] + sal[3] + sal[4];
+      break;
+    case 6:
+      newSalary = sal[0] + sal[1] + sal[2] + "." + sal[3] + sal[4] + sal[5];
+      break;
+  }
+ 
+  //Funciones de Manejo de Roles
   useEffect(() => {
-    getAdmin(params,setRoles);
+    getAdmin(params, setRoles);
   }, []);
   useEffect(() => {
-    if (rols) asignamentRol(admin,options,optionsadmin,optionsemployee,setRols,setMostarOptionsAddRol,getEmployee);
+    if (rols)
+      asignamentRol(
+        admin,
+        options,
+        optionsadmin,
+        optionsemployee,
+        setRols,
+        setMostarOptionsAddRol,
+        getEmployee
+      );
   }, [rols]);
 
   return (
-        <tr>
-          <td style={{ color: "#6c1a6bdc", borderRight: "thin solid #f5f5f5" }}>
-            {index + 1}
-          </td>
-          <td style={{ color: "#05920cdc", borderRight: "thin solid #f5f5f5" }}>
-            {name}
-          </td>
-          <td style={{ color: "#05920cdc", borderRight: "thin solid #f5f5f5" }}>
-            {rol}
-          </td>
-          <td style={{ color: "#071ca8f3", borderRight: "thin solid #f5f5f5" }}>
-            {salary}
-          </td>
-          <td style={{ borderRight: "thin solid #f5f5f5" }}>
-            <button onClick={() => delet(id, index, data,roles,deletEmployee)}>Eliminar</button>
-            <button onClick={() => edit(id, index)}>Editar</button>
-          </td>
-        
-          {
-            roles === import.meta.env.VITE_ROL_Z  &&   <td>
-            <select
-              onChange={(e) => {
-                if (e.target.value !== "Select Rol")
-                  asignament(data, e.target.value,setRols,setAdmin);
-              }}
-              onClick={() => {
-                setMostarOptionsAddRol(true);
-              }}
-            >
-              <option >Select Rol</option>
-              {mostarOptionsAddRol && (
-                <>
+    <tr>
+      <td style={{ color: "#6c1a6bdc", borderRight: "thin solid #f5f5f5" }}>
+        {index + 1}
+      </td>
+      <td style={{ color: "#05920cdc", borderRight: "thin solid #f5f5f5" }}>
+        {name}
+      </td>
+      <td style={{ color: "#05920cdc", borderRight: "thin solid #f5f5f5" }}>
+        {rol}
+      </td>
+      <td style={{ color: "#071ca8f3", borderRight: "thin solid #f5f5f5" }}>
+        {newSalary + "$"}
+      </td>
+      <td style={{ borderRight: "thin solid #f5f5f5" }}>
+        <button onClick={() => delet(id, index, data, roles, deletEmployee)}>
+          Eliminar
+        </button>
+        <button onClick={() => edit(id, index)}>Editar</button>
+      </td>
 
-                {
-                  !data.rol  ? <>
-                  <option value="empleado">Empleado</option> 
-                  <option value="admin">Admin</option>
+      {roles === import.meta.env.VITE_ROL_Z && (
+        <td>
+          <select
+            onChange={(e) => {
+              if (e.target.value !== "Select Rol")
+                asignament(data, e.target.value, setRols, setAdmin);
+            }}
+            onClick={() => {
+              setMostarOptionsAddRol(true);
+            }}
+          >
+            <option>Select Rol</option>
+            {mostarOptionsAddRol && (
+              <>
+                {!data.rol ? (
+                  <>
+                    <option value="empleado">Empleado</option>
+                    <option value="admin">Admin</option>
                   </>
-                  :
-                  data.rol === "admin" ?  <option value="empleado">Empleado</option> :
+                ) : data.rol === "admin" ? (
+                  <option value="empleado">Empleado</option>
+                ) : (
                   <option value="admin">Admin</option>
-                 
-                }
-                </>
-              )}
-            </select>
-          </td>
-          }
-        
-        </tr>
-
+                )}
+              </>
+            )}
+          </select>
+        </td>
+      )}
+    </tr>
   );
 }
 
